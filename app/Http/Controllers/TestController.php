dd95a3f56ed2f5f9ab6a838ce7e8031f3110b49c
andrewozhegov <andrew.ozhegov@yandex.ru>
2017-08-11 23:22:54 +0300

    Added Test Validation

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -11,8 +11,28 @@ class TestController extends Controller
         return view('test');
     }
 
-    public function send()
+    public function send(Request $request)
     {
-        //return view('');
+        $rules = [
+            'answer1' => 'required|in:1,2,3',
+            'answer2' => 'required|numeric',
+            'answer31' => 'required_without_all:answer32,answer33,andwer34,answer35'/*,
+            'answer32' => '',
+            'answer33' => '',
+            'answer34' => '',
+            'answer35' => ''*/
+        ];
+
+        $messages = [
+            'answer1.in' => 'На вопрос 1 не был дан ответ!',
+            'answer2.numeric' => 'Ответом на вопрос 2 должно быть число!',
+            'answer31.required_without_all' => 'Необходимо выбрать хотя бы одно значение!'
+        ];
+
+        $this->validate($request, $rules, $messages);
+
+        // тут будет верификация данных и выставление оценки
+
+        // занесение оценки в базу данных
     }
 }
--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -18,7 +18,19 @@
     <section class="content container">
         <h1 style="margin: 50px"> Тест</h1>
 
+        @if (count($errors) > 0)
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+
         <form id="form" class="form-horizontal" role="form" method="post">
+
+            {{ csrf_field() }}
 
             <div class="form-group" style="margin: 50px">
                 <div class="col-md-12">
@@ -26,10 +38,10 @@
                 </div>
                 <div class="col-md-12">
                     <select name="answer1" id="form_answer1" class="form-control" required>
-                        <option value="error" selected>Не знаю</option>
-                        <option value="false">Квадрат</option>
-                        <option value="true">Куб</option>
-                        <option value="false">Гиперкуб</option>
+                        <option value="0" selected>Не знаю</option>
+                        <option value="1">Квадрат</option>
+                        <option value="2">Куб</option>
+                        <option value="3">Гиперкуб</option>
                     </select>
                 </div>
             </div>
@@ -50,23 +62,23 @@
                 </div>
                 <div class="col-md-12">
                     <label class="checkbox-inline">
-                        <input type="checkbox" name="answer3" id="form_answer32" value="false">
+                        <input type="checkbox" name="answer31" id="form_answer32" value="1">
                         1/2
                     </label>
                     <label class="checkbox-inline">
-                        <input type="checkbox" name="answer3" id="form_answer32" value="true">
+                        <input type="checkbox" name="answer32" id="form_answer32" value="1">
                         0
                     </label>
                     <label class="checkbox-inline">
-                        <input type="checkbox" name="answer3" id="form_answer32" value="true">
+                        <input type="checkbox" name="answer33" id="form_answer32" value="1">
                         4
                     </label>
                     <label class="checkbox-inline">
-                        <input type="checkbox" name="answer3" id="form_answer32" value="false">
+                        <input type="checkbox" name="answer34" id="form_answer32" value="1">
                         -6
                     </label>
                     <label class="checkbox-inline">
-                        <input type="checkbox" name="answer3" id="form_answer32" value="false">
+                        <input type="checkbox" name="answer35" id="form_answer32" value="1">
                         3.1
                     </label>
                 </div>
